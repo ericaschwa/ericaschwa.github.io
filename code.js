e0459d82a0fcbcfc57c96969aec366a199301ed9
Erica Schwartz <Erica_S.Schwartz@tufts.edu>
2016-07-16 10:58:48 -0700

    update to resume

--- code.js
+++ code.js
@@ -24,24 +24,32 @@ app.controller('resumeCtrl', function($scope) {
 		'Theory of Computation'
     ];
     $scope.experience = [
-    	{'title': 'Software Engineering Intern at MITRE, Summer 2015', 'tasks': [
+        {'title': 'Software Engineering Intern at SurveyMonkey, Summer 2016 (Present)',
+         'tasks': [
+            'Create an event log to display the activity of a given account on the site',
+            'Utilize tools such as RabbitMQ, Pyramid, and Jinja2 to create a complete, full-stack product'
+        ]},
+    	{'title': 'Software Engineering Intern at MITRE, Summer 2015',
+         'tasks': [
     		'Prototyped a modern, thin client user interface for an existing system of record',
 			'Presented project to the upper level management at the Intern Innovation Brown Bag',
             'Practiced agile scrum methods for collaboration with a software development team'
     	]},
-    	{'title': 'Intern at the Social Perception Lab at Dartmouth College, Summer 2012 and Summer 2014', 'tasks': [
+    	{'title': 'Intern at the Social Perception Lab at Dartmouth College, Summer 2012 and Summer 2014',
+         'tasks': [
     		'Adapted object and face recognition tests to be accessible online',
 			'Developed a website on the topic of prosopagnosia in kids for parents and researchers'
     	]},
-    	{'title': 'Intern at the Cancer Risk Behaviors Group at Dartmouth College, Summer 2013', 'tasks': [
+    	{'title': 'Intern at the Cancer Risk Behaviors Group at Dartmouth College, Summer 2013',
+         'tasks': [
     		'Conducted a literature review resulting in a 72-article Endnote library',
 			"Analyzed content of fast food restaurants’ and alcohol brands’ websites"
     	]}
     ];
-    $scope.software= 'MS Office, Photoshop, SPSS, Git, Linux';
+    $scope.software= 'Photoshop, SPSS, Git, Linux';
     $scope.languages = 'C, C++, Python, HTML/CSS, Javascript/AngularJS';
     $scope.activities=[
-    	'Teaching Assistant and Lab Leader for Computer Science 0015 (a.k.a. "Data Structures")',
+    	'Senior Teaching Assistant and Lab Leader for Computer Science 0015 (a.k.a. "Data Structures")',
 		'Project Leader and Programmer for JumboCode',
 		'Leadership Council member for Peer Health Exchange'
     ];
