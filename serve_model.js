cd7f6b32b48dac9ce3eba6607a6e4f9916aba576
Erica Schwartz <Erica_S.Schwartz@tufts.edu>
2020-12-21 21:27:46 -0800

    comment everything out

--- serve_model.js
+++ serve_model.js
@@ -3,16 +3,16 @@ console.log("is import the problem?")
 import * as tf from '@tensorflow/tfjs';
 console.log("?")
 
-my_function = tf.loadLayersModel('model.json');
-console.log("loaded")
+// my_function = tf.loadLayersModel('model.json');
+// console.log("loaded")
 
-state = []
-for (i = 0; i < 1240; i++) {
-	state.push(0)
-}
-possible_actions = []
-for (i = 0; i < 180; i++) {
-	possible_actions.push(0)
-{
-console.log("will make prediction")
+// state = []
+// for (i = 0; i < 1240; i++) {
+// 	state.push(0)
+// }
+// possible_actions = []
+// for (i = 0; i < 180; i++) {
+// 	possible_actions.push(0)
+// {
+// console.log("will make prediction")
 // prediction = m.predict([state, possible_actions]);
