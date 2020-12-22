6781944ecd16d83a1e84391019308ff0a7ad38d2
Erica Schwartz <Erica_S.Schwartz@tufts.edu>
2020-12-21 21:34:40 -0800

    add async func

--- serve_model.js
+++ serve_model.js
@@ -1,16 +1,20 @@
 // JavaScript
 console.log("?");
-
-my_function = tf.loadLayersModel('model.json');
 console.log("loaded");
 
-state = []
-for (i = 0; i < 1240; i++) {
-	state.push(0)
+async function testModel() {
+    const model = await tf.loadLayersModel('model.json');
+    state = [];
+	for (i = 0; i < 1240; i++) {
+		state.push(0);
+	}
+	possible_actions = [];
+	for (i = 0; i < 180; i++) {
+		possible_actions.push(0);
+	}
+	console.log("will make prediction");
 }
-possible_actions = []
-for (i = 0; i < 180; i++) {
-	possible_actions.push(0)
-{
-console.log("will make prediction")
+
+testModel();
+
 // prediction = m.predict([state, possible_actions]);
